375c39a4c6ce43ac5e207a1bad5c1df106ade315
Steve <steve@gocardless.com>
2013-03-07 13:13:36 +0000

    Better PATH

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,12 +1,5 @@
-# PATH
-export PATH=$HOME/.rbenv/bin:$PATH
-
-export PATH=$PATH:/usr/local/share/npm/bin
-
-export PATH=$PATH:$HOME/src/go/bin
-
 # Customize to your needs...
-export PATH=$PATH:$HOME/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/sbin:/bin:/usr/X11/bin
+export PATH=$HOME/.rbenv/bin:/usr/local/share/npm/bin:$HOME/src/go/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/sbin:/bin:/usr/X11/bin
 
 # NODE PATH
 export NODE_PATH=/usr/local/lib/node
