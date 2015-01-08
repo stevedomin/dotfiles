b0b70ce95e9c9cf0353bc2ac22a35a8a68918045
Steve <steve@stevedomin.com>
2015-01-08 15:08:40 +0000

    Always reset binstubs path before setting a new one

--- src/dot.config/fish/plugins/bundler/bundler.fish
+++ src/dot.config/fish/plugins/bundler/bundler.fish
@@ -13,6 +13,7 @@ function __bundler_set_binstub_path --on-event fish_prompt
   if test -z $gemfile_path
     __bundle_reset_binstub_path
   else
+    __bundle_reset_binstub_path
     __bundle_set_binstub_path $gemfile_path
   end
 end
