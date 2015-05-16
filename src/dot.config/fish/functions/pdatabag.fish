0d140a99d081aad64c016fb08c4e3d3a0552d5a7
Steve <steve@stevedomin.com>
2015-05-16 13:39:28 +0100

    Better knife databag functions

--- src/dot.config/fish/functions/pdatabag.fish
+++ src/dot.config/fish/functions/pdatabag.fish
@@ -1,5 +1,9 @@
 function pdatabag
   set KNIFE_ENV 'production'
-  command knife data bag --secret-file ~/.chef/gocardless/production/encrypted_data_bag_secret $argv
+  if not contains 'list' $argv
+    command knife data bag --secret-file ~/.chef/gocardless/production/encrypted_data_bag_secret $argv
+  else
+    command knife data bag $argv  
+  end
   set KNIFE_ENV 'staging'
 end
--- src/dot.config/fish/functions/sdatabag.fish
+++ src/dot.config/fish/functions/sdatabag.fish
@@ -1,4 +1,8 @@
 function sdatabag
   set KNIFE_ENV 'staging'
-  command knife data bag --secret-file ~/.chef/gocardless/staging/encrypted_data_bag_secret $argv
+  if not contains 'list' $argv
+    command knife data bag --secret-file ~/.chef/gocardless/staging/encrypted_data_bag_secret $argv
+  else
+    command knife data bag $argv  
+  end
 end
