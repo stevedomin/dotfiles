56176eabff55e26e2922cf187cd6174a6c49bee4
Steve <steve@stevedomin.com>
2015-06-12 18:24:59 +0100

    Remove crazy databag function

--- src/dot.config/fish/functions/pdatabag.fish
+++ src/dot.config/fish/functions/pdatabag.fish
@@ -1,9 +1,5 @@
 function pdatabag
   set KNIFE_ENV 'production'
-  if not contains 'list' $argv
-    command knife data bag --secret-file ~/.chef/gocardless/production/encrypted_data_bag_secret $argv
-  else
-    command knife data bag $argv  
-  end
+  command knife data bag $argv  
   set KNIFE_ENV 'staging'
 end
--- src/dot.config/fish/functions/sdatabag.fish
+++ src/dot.config/fish/functions/sdatabag.fish
@@ -1,8 +1,4 @@
 function sdatabag
   set KNIFE_ENV 'staging'
-  if not contains 'list' $argv
-    command knife data bag --secret-file ~/.chef/gocardless/staging/encrypted_data_bag_secret $argv
-  else
-    command knife data bag $argv  
-  end
+  command knife data bag $argv  
 end
