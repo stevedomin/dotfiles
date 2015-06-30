0722603c2428532933b0de33e8e5546ae3812940
Steve <steve@stevedomin.com>
2015-06-30 19:39:07 +0100

    Move aliases to separate file

--- src/dot.config/fish/config.fish
+++ src/dot.config/fish/config.fish
@@ -1,5 +1,7 @@
 set fish_greeting
 
+. ~/.config/fish/aliases.fish
+
 # --------------------------------------------
 # ENV
 # --------------------------------------------
@@ -25,18 +27,6 @@ set -xg PATH /usr/local/bin $PATH
 set -xg PATH /usr/local/share/npm/bin $PATH
 set -xg PATH $HOME/src/go/bin $PATH
 set -xg PATH $GOBIN $PATH
-
-# --------------------------------------------
-# ALIASES
-# --------------------------------------------
-
-alias ldatabag "lknife data bag $argv"
-alias pdatabag "pknife data bag $argv"
-alias sdatabag "sknife data bag $argv"
-
-alias lssh "lknife ssh -C 10 -x sdomin -a softlayer.private_ip --ssh-port 61315 $argv"
-alias pssh "pknife ssh -C 10 -x sdomin -a softlayer.private_ip --ssh-port 61315 $argv"
-alias sssh "sknife ssh -C 10 -x sdomin -a softlayer.private_ip --ssh-port 61315 $argv"
 
 # --------------------------------------------
 # RBENV
